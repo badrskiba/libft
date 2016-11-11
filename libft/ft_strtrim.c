c0cff9ff33e10bdd5db4e9939e031bbbb612e978
Badr SKIBA <bskiba@e3r10p12.42.fr>
2016-11-11 21:52:19 +0100

    libft

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,39 +6,37 @@
 /*   By: bskiba <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/11 12:09:15 by bskiba            #+#    #+#             */
-/*   Updated: 2016/11/11 13:17:32 by bskiba           ###   ########.fr       */
+/*   Updated: 2016/11/11 21:42:46 by bskiba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s)
+char		*ft_strtrim(char const *s)
 {
-	char *tmp;
-	int i;
-	int j;
-	int k;
+	char	*tmp;
+	int		i;
+	int		j;
+	int		k;
 
-	if (*s)
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+	k = 0;
+	j = ft_strlen(s);
+	tmp = (char*)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (tmp == NULL)
+		return (NULL);
+	while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
+		i++;
+	while ((s[j - 1] == ' ' || s[j - 1] == '\n' || s[j - 1] == '\t'))
+		j--;
+	while (i < j)
 	{
-		i = 0;
-		k = 0;
-		j = ft_strlen(s);
-		tmp = (char*)malloc(sizeof(char) * (ft_strlen(s) + 1));
-		if (tmp == NULL)
-			return (NULL);
-		while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
-			i++;
-		while (s[j] == ' ' || s[j] == '\n' || s[j] == '\t')
-			j--;
-		while (i <= j)
-		{
-			tmp[k] = s[i];
-			i++;
-			k++;
-		}
-		tmp[k] = '\0';
-		return ((char*)s);
+		tmp[k] = s[i];
+		i++;
+		k++;
 	}
-	return (NULL);
+	tmp[k] = '\0';
+	return ((char*)tmp);
 }
